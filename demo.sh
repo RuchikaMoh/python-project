498f2248178cba7bf3cb95be73d03c99adcfc459
RuchikaMoh <169063062+RuchikaMoh@users.noreply.github.com>
2025-07-11 20:15:48 +0530

    Update demo.sh

--- demo.sh
+++ demo.sh
@@ -6,4 +6,4 @@ sudo systemctl start apache2
 sudo systemctl enable apache2
 sudo systemctl status apache2
 
-echo "this is shell scrpit" >> /var/www/html/index.html
+echo "this is shell scrpit" >> /var/www/html/demo.sh
