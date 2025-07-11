1acedc52c7e3cfc5945af113678a62dd11be86fb
RuchikaMoh <169063062+RuchikaMoh@users.noreply.github.com>
2025-07-11 19:36:27 +0530

    Create demo.sh

---
+++ demo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+apt-get update -y 
+apt-get install apache2
+systemctl start apache2
+systemctl enable apache2
+systemctl status apache2
+
+echo "this is shell scrpit" >> /var/www/html/index.html
